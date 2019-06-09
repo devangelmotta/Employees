31b413538ea5b1e11e08bb99a28d666585d380ef
Angel Motta <devangelmotta@hotmail.com>
2019-06-08 21:09:58 -0500

    app

--- backend/database.js
+++
@@ -1,5 +0,0 @@
-// database
-
-module.exports = {
-    db: 'mongodb://localhost:27017/db-employees'
- };
--- src/app/models/Employees.ts
+++
@@ -1,8 +0,0 @@
-// Employees.ts
-
-export default class Employees {
-    employee_name: String;
-    employee_salary: String;
-    employee_age: Number;
-    profile_image: String;
-  }
